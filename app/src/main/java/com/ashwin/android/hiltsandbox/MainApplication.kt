30ab57b392468567427589004067e6a683603576
Ashwin <ashwin.dinesh01@gmail.com>
2021-07-03 19:45:14 +0530

    Add comment

--- app/src/main/java/com/ashwin/android/hiltsandbox/MainApplication.kt
+++ app/src/main/java/com/ashwin/android/hiltsandbox/MainApplication.kt
@@ -3,5 +3,8 @@ package com.ashwin.android.hiltsandbox
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * This is necessary for Hilt
+ */
 @HiltAndroidApp
 class MainApplication : Application()
